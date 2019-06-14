23333684b2a5a48b6f8fd75a007b59fd65027400
eleven <599030042@qq.com>
2019-06-14 13:15:15 +0800

    ds

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexHome from '../components/index/Home.vue'
+import EnergyHome from '../components/energy/Home.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path:'/',name: 'IndexHome',component:IndexHome },
+    { path:'/energy',name: 'EnergyHome',component: EnergyHome}
   ]
 })
